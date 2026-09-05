349a50bf6d11ada5483a55470ca494cf0c600c29
Prachika Dhaubhadel <prachika8125@student.ku.edu.np>
2026-09-05 12:37:19 +0545

    Connect route to create travel memory

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const authRoutes = require('./routes/auth'); // Import auth routes
+const memoryRoutes = require('./routes/memories'); // Import travel entry routes
 const app = express();
 
 // Middleware to parse JSON and handle cross-origin requests
@@ -11,6 +12,8 @@ app.use(cors());
 
 // Mount the route: any request sent to /api/auth/* goes to routes/auth.js
 app.use('/api/auth', authRoutes);
+// Mount the route: any request sent to /api/memories/* goes to routes/memories.js
+app.use('/api/memories', memoryRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
